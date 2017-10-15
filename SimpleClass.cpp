f78d0b421154b1b2e7bae11edec32a51e9a8ed7d
pa1511 <peafric@gmail.com>
2017-10-15 12:50:24 +0200

    First commit

---
+++ SimpleClass.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   SimpleClass.cpp
+ * Author: paf
+ * 
+ * Created on October 15, 2017, 12:48 PM
+ */
+
+#include "SimpleClass.h"
+
+SimpleClass::SimpleClass() {
+}
+
+SimpleClass::SimpleClass(const SimpleClass& orig) {
+}
+
+SimpleClass::~SimpleClass() {
+}
+
---
+++ SimpleClass.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   SimpleClass.h
+ * Author: paf
+ *
+ * Created on October 15, 2017, 12:48 PM
+ */
+
+#ifndef SIMPLECLASS_H
+#define SIMPLECLASS_H
+
+class SimpleClass {
+public:
+    SimpleClass();
+    SimpleClass(const SimpleClass& orig);
+    virtual ~SimpleClass();
+private:
+
+};
+
+#endif /* SIMPLECLASS_H */
+
