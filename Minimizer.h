d7d3a934656032fdb403b2b3d33de43874d6fdf8
pa1511 <peafric@gmail.com>
2018-01-16 20:20:07 +0100

    Added documentation for Minimizer

--- Minimizer.h
+++ Minimizer.h
@@ -15,19 +15,47 @@
 #define MINIMIZER_H
 
 #pragma pack(push, 1)
+
+/**
+ * Single minimizer data package. <br/>
+ * A minimizer contains two values: <br/>
+ * <ul>
+ * <li> m - minimizer hash value </li>
+ * <lI> i - position in the sequence </li>
+ * </ul>
+ */
 struct Minimizer {
     int m;
     int i;
+
+    /*
+     * Comparison operators implemented so minimizers can be easily compared 
+     */
     
-    bool operator <(const Minimizer& other) const {
+    bool operator<(const Minimizer& other) const {
         if (this->m == other.m) {
             return this->i < other.i;
         }
         return this->m < other.m;
     }
-    
-    bool operator ==(const Minimizer& other) const {
-        return this->m==other.m && this->i==other.i;
+
+    bool operator>(const Minimizer& other) const {
+        if (this->m == other.m) {
+            return this->i > other.i;
+        }
+        return this->m > other.m;
+    }
+
+    bool operator==(const Minimizer& other) const {
+        return this->m == other.m && this->i == other.i;
+    }
+
+    bool operator<=(const Minimizer& other) const {
+        return *this < other || *this == other;
+    }
+
+    bool operator>=(const Minimizer& other) const {
+        return *this > other || *this == other;
     }
 
 };
